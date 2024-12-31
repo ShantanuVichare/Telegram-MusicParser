6f9ca1d02cab7ee27d644407ceda1c9b83015f58
ShantanuVichare <sheradgrr54@gmail.com>
2024-12-31 00:07:56 -0500

    updated build script

--- build.sh
+++ build.sh
@@ -2,7 +2,35 @@
 # Exit on error
 set -o errexit
 
-conda env create -f environment.yml -n music-parser
+uname -a
+
+# Remove existing miniconda installation
+if [ -d "$HOME/miniconda" ];
+then
+    echo "Existing miniconda installation found"
+    # echo "Removing existing miniconda installation"
+    # rm -rf $HOME/miniconda
+else
+    echo "No existing miniconda installation found... Installing Miniconda"
+    # Install Miniconda
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -q -O miniconda.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+fi
+
+export PATH="$HOME/miniconda/bin:$PATH"
+conda init bash
+source ~/.bashrc
+
+# Check if the conda environment already exists
+if conda env list | grep -q 'music-parser'; then
+    echo "Conda environment 'music-parser' already exists. Updating environment."
+    conda env update -f environment.yml -n music-parser
+else
+    echo "Conda environment 'music-parser' does not exist. Creating new environment."
+    conda env create -f environment.yml -n music-parser
+fi
+
+# Activate conda environment
 conda activate music-parser
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
